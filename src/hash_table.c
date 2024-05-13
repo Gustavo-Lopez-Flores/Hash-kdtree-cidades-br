eab3d31420f1ce189d23d2b80680906c24c2bb52
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-05-12 20:59:00 -0400

    funcao insere cidade

--- src/hash_table.c
+++ src/hash_table.c
@@ -3,11 +3,26 @@
 #include <string.h>
 #include "../include/hash_table.h"
 
-#define PRIMO 131
-#define PRIMO 137
+#define PRIMO1 131
+#define PRIMO2 137
 
 void constroi_hash(Hashtable *hash_table, int capacidade) {
     hash_table->cidades = (Cidade *)malloc(capacidade * sizeof(Cidade));
     hash_table->capacidade = capacidade;
     hash_table->tamanho = 0;
+}
+
+int funcao_hash(int codigo_ibge, int capacidade, int tentativa) {
+    return (codigo_ibge % capacidade + tentativa * (PRIMO1 - (codigo_ibge % PRIMO2))) % capacidade;
+}
+
+void insere_cidade(Hashtable *hash_table, Cidade cidade) {
+    int index = hash_function(cidade.codigo_ibge, hash_table->capacidade, 0);
+
+    while (hash_table->cidades[index].codigo_ibge != 0) {
+        index = (index + 1) % hash_table->capacidade;
+    }
+
+    hash_table->cidades[index] = cidade;
+    hash_table->tamanho++;
 }
