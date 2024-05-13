0a60edd1ec3eba15506883c460dc601279a1efc6
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-05-12 20:38:22 -0400

    HashTable armazenará um 'vetor' de cidades, por isso a fusão das entidades

--- include/hash_table.h
+++ include/hash_table.h
@@ -1,16 +1,22 @@
-#include "cidade.h"
 #ifndef __HASH_TABLE__
 #define __HASH_TABLE__
 
-#define TAMANHO 6000
+typedef struct {
+    int codigo_ibge;
+    char nome[100];
+    float latitude;
+    float longitude;
+    int capital;
+    int codigo_uf;
+    int siafi_id;
+    int ddd;
+    char fuso_horario[100];
+} Cidade;
 
 typedef struct {
-    int chave;
-    Cidade cidade;
-} Objeto;
-
-typedef struct {
-    Objeto *table;
-} HashTable;
+    Cidade *cidades;
+    int capacidade;
+    int tamanho;
+} Hashtable;
 
 #endif
