2a20e4d90edab8ec167fc2e0fbd72ce519186862
Gustavo Lopez Flores <contatogustavolopezflores@gmail.com>
2024-05-12 20:15:28 -0400

    struct e tamanho da tabela hash

--- include/hash_table.h
+++ include/hash_table.h
@@ -1,4 +1,14 @@
+#include "cidade.h"
 #ifndef __HASH_TABLE__
 #define __HASH_TABLE__
+
+#define TAMANHO 6000
+
+typedef struct {
+    int chave;
+    Cidade cidade;
+} thash;
+
+thash tabela_hash[TAMANHO];
 
 #endif
